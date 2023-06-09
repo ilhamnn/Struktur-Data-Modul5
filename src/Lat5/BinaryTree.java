f28db3a92bf09bb49008459b8839856e7a4fabb4
ilhamnn <127362918+ilhamnn@users.noreply.github.com>
2023-06-09 16:52:44 +0700

    Add files via upload

---
+++ src/Lat5/BinaryTree.java
@@ -0,0 +1,57 @@
+package Lat5;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class BinaryTree {
+    Node root;
+
+    public void NewNode(int data) {
+        root = NewNode(root, new Node(data));
+    }
+
+    public Node NewNode(Node root, Node newData) {
+        if (root == null) {
+            root = newData;
+            return root;
+        }
+
+        if (newData.data < root.data) {
+            root.left = NewNode(root.left, newData);
+        } else {
+            root.right = NewNode(root.right, newData);
+        }
+        return root;
+    }
+
+    public void preOrder(Node node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+
+    public void postOrder(Node node) {
+        if (node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
+}
